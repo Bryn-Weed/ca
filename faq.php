456b6831c1b92026db808afb2923df177cc8c359
Bryn-Weed <36001253+Bryn-Weed@users.noreply.github.com>
2018-11-27 15:03:42 +0000

    Initial commit

---
+++ faq.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+    <title>FAQ</title>
+
+    <link rel="stylesheet" href="style.css" type="text/css" />
+    <link rel="stylesheet" href="styling.css" type="text/css" />
+
+
+</head>
+
+<body onload="setInterval('chat.update()', 1000)">
+
+<div id="page-wrap">
+<h2>Frequently Asked Questions</h2>
+
+  <div class="row">
+<div class="col-md-4">
+
+        <button class="accordion">Can I pay in person?</button>
+        <div class="panel">
+          <p>You absolutely can. When requesting a service, you can ignore the payment section if you would prefer to pay the staff when they arrive.</p><br>
+        </div><br>
+
+        <button class="accordion">Do I have to order online?</button>
+        <div class="panel">
+          <p>You can also order services over the phone or via email, the contact details are shown in the section below.</p><br>
+        </div><br>
+
+        <button class="accordion">My dog doesn't walk well with others, can it be walked separately?</button>
+        <div class="panel">
+          <p>Absolutely. There is a 'Notes' section when requesting a service, where you can let us known of any specific details regarding your request.</p><br>
+        </div><br>
+
+        <button class="accordion">My dog has a very specific diet, how will this be handled?</button>
+        <div class="panel">
+          <p>Let us know in the "Notes" section when adding a service to your cart. From there we can negotiate how it will be handled; you can either provide the food yourself or we can find it as well.</p><br>
+        </div><br>
+
+        <button class="accordion">I don't own a lead or any toys for my dog, what can I do?</button>
+        <div class="panel">
+          <p>Canine Adventures can provide leads and toys for dog walking and training.</p><br>
+        </div><br>
+        <h5><font color="#eee"> If your question has not been answered, try asking in the chat forum below:</font> </h5>
+</div>
+
+
+
+<br><br><Br>
+      
+</body>
+
+</html>
